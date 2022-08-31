92259e79b7e223b284e6ab922e799d2ee0714ca9
Manolo Carrasco <manolo@vaadin.com>
2022-08-31 10:44:28 +0200

    add pois to map

--- src/main/java/com/vaadin/example/sightseeing/views/map/MapView.java
+++ src/main/java/com/vaadin/example/sightseeing/views/map/MapView.java
@@ -1,8 +1,13 @@
 package com.vaadin.example.sightseeing.views.map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.vaadin.example.sightseeing.data.generator.DataGenerator;
+import com.vaadin.example.sightseeing.data.service.PlaceRepository;
 import com.vaadin.flow.component.map.Map;
+import com.vaadin.flow.component.map.configuration.Coordinate;
 import com.vaadin.flow.component.map.configuration.View;
+import com.vaadin.flow.component.map.configuration.feature.MarkerFeature;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -17,13 +22,20 @@ public class MapView extends VerticalLayout {
 
     private Map map = new Map();
 
-    public MapView() {
+
+    @Autowired
+    public MapView(PlaceRepository repo) {
         setSizeFull();
         setPadding(false);
         map.getElement().setAttribute("theme", "borderless");
         View view = map.getView();
         view.setCenter(DataGenerator.CENTER);
         view.setZoom(12);
+        repo.findAll().forEach(place -> {
+            MarkerFeature feat = new MarkerFeature(new Coordinate(place.getX(), place.getY()),
+                    MarkerFeature.POINT_ICON);
+            map.getFeatureLayer().addFeature(feat);
+        });
         addAndExpand(map);
     }
 }
