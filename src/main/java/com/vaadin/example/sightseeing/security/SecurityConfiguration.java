0dd7a69e9e7bafc77bd9fa05a5b79817823c3006
Manolo Carrasco <manolo@vaadin.com>
2022-09-02 18:44:25 +0200

    configure login to be stateless

--- src/main/java/com/vaadin/example/sightseeing/security/SecurityConfiguration.java
+++ src/main/java/com/vaadin/example/sightseeing/security/SecurityConfiguration.java
@@ -1,20 +1,34 @@
 package com.vaadin.example.sightseeing.security;
 
-import com.vaadin.example.sightseeing.views.login.LoginView;
-import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+import java.util.Base64;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
+
+import com.vaadin.example.sightseeing.views.login.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
 
     public static final String LOGOUT_URL = "/";
+
+    // The secret is stored in /config/secrets/application.properties by default.
+    // Never commit the secret into version control; each environment should have
+    // its own secret.
+    @Value("${com.vaadin.example.sightseeing.auth.secret}")
+    private String authSecret;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -23,9 +37,12 @@ public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         super.configure(http);
         setLoginView(http, LoginView.class, LOGOUT_URL);
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        setStatelessAuthentication(http, new SecretKeySpec(Base64.getDecoder().decode(authSecret), JwsAlgorithms.HS256),
+                "com.vaadin.example.sightseeing");
     }
 
     @Override
