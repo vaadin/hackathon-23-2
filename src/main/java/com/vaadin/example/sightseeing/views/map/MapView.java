0f703589d5acb94f70c77e1a4d9226c17236523d
Herberts <herberts@vaadin.com>
2022-09-05 17:42:35 +0300

    Add center on user button

--- src/main/java/com/vaadin/example/sightseeing/views/map/MapView.java
+++ src/main/java/com/vaadin/example/sightseeing/views/map/MapView.java
@@ -2,11 +2,15 @@ package com.vaadin.example.sightseeing.views.map;
 
 import com.vaadin.example.sightseeing.data.generator.DataGenerator;
 import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.map.Map;
 import com.vaadin.flow.component.map.configuration.Coordinate;
 import com.vaadin.flow.component.map.configuration.View;
 import com.vaadin.flow.component.map.configuration.feature.MarkerFeature;
 import com.vaadin.flow.component.map.configuration.style.Icon;
+import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -26,6 +30,7 @@ public class MapView extends VerticalLayout {
     private Map map = new Map();
     private MarkerFeature userMarker;
     private boolean initialCoordinates = true;
+    private MenuBar menuBar;
 
 
     public MapView() {
@@ -36,6 +41,16 @@ public class MapView extends VerticalLayout {
         view.setCenter(DataGenerator.CENTER);
         view.setZoom(12);
         addAndExpand(map);
+
+        var centerOnMeButton = new Button(VaadinIcon.DOT_CIRCLE.create());
+        centerOnMeButton.addClickListener(event -> centerOnUser());
+        centerOnMeButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        centerOnMeButton.getStyle().set("background-color", "#e2efdd")
+                .set("color", "#73877e")
+                .set("position", "absolute")
+                .set("top", "10px")
+                .set("right", "10px");
+        add(centerOnMeButton);
 
         userMarker = new MarkerFeature();
         var userIconOptions = new Icon.Options();
@@ -56,9 +71,17 @@ public class MapView extends VerticalLayout {
     public void refreshUserMarker(GeolocationCoordinates coords) {
         userMarker.setCoordinates(new Coordinate(coords.getLongitude(), coords.getLatitude()));
         if (initialCoordinates) {
+            initialCoordinates = false;
             map.getFeatureLayer().addFeature(userMarker);
             // Updates center to the user location, just once.
-            map.setCenter(userMarker.getCoordinates());
+            centerOnUser();
+        }
+    }
+
+    private void centerOnUser() {
+        var coordinates = userMarker.getCoordinates();
+        if (coordinates != null && coordinates.getX() != 0 && coordinates.getY() != 0) {
+            map.setCenter(coordinates);
         }
     }
 }
